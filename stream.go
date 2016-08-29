f9c8fc37e157107c387fb6ade55b2345de09e4ac
xtaci <daniel820313@gmail.com>
2016-08-29 21:09:05 +0800

    upd

--- stream.go
+++ stream.go
@@ -2,17 +2,19 @@ package smux
 
 import (
 	"bytes"
-	"errors"
 	"log"
 	"net"
 	"sync"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 const (
 	streamIdle = 1 << iota
 	streamSynSent
 	streamEstablished
+	streamClosed
 )
 
 const (
@@ -21,14 +23,17 @@ const (
 
 // Stream implements io.ReadWriteCloser
 type Stream struct {
-	id     uint32
-	state  int
-	buffer bytes.Buffer
-	chRx   chan Frame
-	fr     Framer
-	qdisc  Qdisc
-	mu     sync.Mutex
-	die    chan struct{}
+	id               uint32
+	state            int
+	buffer           bytes.Buffer
+	chRx             chan Frame
+	fr               Framer
+	qdisc            Qdisc
+	mu               sync.Mutex
+	die              chan struct{}
+	chNotifyReadable chan struct{}
+	readDeadline     time.Time
+	writeDeadline    time.Time
 }
 
 func newStream(id uint32, fr Framer, qdisc Qdisc) *Stream {
@@ -39,12 +44,16 @@ func newStream(id uint32, fr Framer, qdisc Qdisc) *Stream {
 	s.state = streamIdle
 	s.chRx = make(chan Frame, streamRxQueueLimit)
 	s.die = make(chan struct{})
+	s.chNotifyReadable = make(chan struct{}, 1)
 	go s.monitor()
 	return s
 }
 
 // Read implements io.ReadWriteCloser
 func (s *Stream) Read(b []byte) (n int, err error) {
+	if s.buffer.Len() > 0 {
+		return s.buffer.Read(b)
+	}
 	return 0, nil
 }
 
@@ -73,7 +82,13 @@ func (s *Stream) Write(b []byte) (n int, err error) {
 
 // Close implements io.ReadWriteCloser
 func (s *Stream) Close() error {
-	close(s.die)
+	select {
+	case <-s.die:
+		return errors.New("broken pipe")
+	default:
+		s.state = streamClosed
+		close(s.die)
+	}
 	return nil
 }
 
@@ -89,18 +104,26 @@ func (s *Stream) RemoteAddr() net.Addr {
 	return nil
 }
 
+// SetDeadline sets the read and write deadlines
+func (s *Stream) SetDeadline(t time.Time) error {
+	if err := s.SetReadDeadline(t); err != nil {
+		return err
+	}
+	if err := s.SetWriteDeadline(t); err != nil {
+		return err
+	}
+	return nil
+}
+
 // SetReadDeadline sets the deadline for future Read calls.
 func (s *Stream) SetReadDeadline(t time.Time) error {
+	s.readDeadline = t
 	return nil
 }
 
 // SetWriteDeadline sets the deadline for future Write calls
 func (s *Stream) SetWriteDeadline(t time.Time) error {
-	return nil
-}
-
-// SetDeadline sets the read and write deadlines
-func (s *Stream) SetDeadline(t time.Time) error {
+	s.writeDeadline = t
 	return nil
 }
 
@@ -112,11 +135,13 @@ func (s *Stream) monitor() {
 			switch s.state {
 			case streamEstablished:
 				if f.options&flagRST != 0 {
-					s.Close()
+					s.state = streamClosed
+					log.Println("connection reset", f.options)
 				} else {
 					if n, err := s.buffer.Write(f.payload); err != nil {
 						log.Println(n, err)
 					}
+					s.notifyReadable()
 				}
 			case streamSynSent:
 				if f.options&flagACK != 0 {
@@ -124,10 +149,21 @@ func (s *Stream) monitor() {
 					if n, err := s.buffer.Write(f.payload); err != nil {
 						log.Println(n, err)
 					}
+					s.notifyReadable()
 				} else {
-					s.Close()
+					s.state = streamClosed
+					log.Println("incorrect packet", f.options)
 				}
 			}
+		case <-s.die:
+			return
 		}
+	}
+}
+
+func (s *Stream) notifyReadable() {
+	select {
+	case s.chNotifyReadable <- struct{}{}:
+	default:
 	}
 }
