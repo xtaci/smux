ac8acc040c26446586db9d975f31763955802517
xtaci <daniel820313@gmail.com>
2016-09-03 17:53:26 +0800

    use bytes.Buffer instead of []byte

--- session.go
+++ session.go
@@ -1,6 +1,7 @@
 package smux
 
 import (
+	"bytes"
 	"io"
 	"sync"
 	"sync/atomic"
@@ -30,9 +31,9 @@ type Session struct {
 	bucket     int32      // token bucket
 	bucketCond *sync.Cond // used for waiting for tokens
 
-	streamBuffers map[uint32][]byte  // stream input buffer
-	streams       map[uint32]*Stream // all streams in this session
-	streamLock    sync.Mutex         // locks streams && frameQueues
+	streamBuffers map[uint32]*bytes.Buffer // stream input buffer
+	streams       map[uint32]*Stream       // all streams in this session
+	streamLock    sync.Mutex               // locks streams && frameQueues
 
 	die            chan struct{} // flag session has died
 	dieLock        sync.Mutex
@@ -48,7 +49,7 @@ func newSession(config *Config, conn io.ReadWriteCloser, client bool) *Session {
 	s.conn = conn
 	s.config = config
 	s.streams = make(map[uint32]*Stream)
-	s.streamBuffers = make(map[uint32][]byte)
+	s.streamBuffers = make(map[uint32]*bytes.Buffer)
 	s.chAccepts = make(chan *Stream, defaultAcceptBacklog)
 	s.chClosedStream = make(chan uint32, defaultCloseWait)
 	s.bucket = int32(config.MaxReceiveBuffer)
@@ -72,9 +73,11 @@ func (s *Session) OpenStream() (*Stream, error) {
 
 	sid := atomic.AddUint32(&s.nextStreamID, 2)
 	stream := newStream(sid, s.config.MaxFrameSize, s)
+	streamBuffer := new(bytes.Buffer)
 
 	s.streamLock.Lock()
 	s.streams[sid] = stream
+	s.streamBuffers[sid] = streamBuffer
 	s.streamLock.Unlock()
 
 	s.writeFrame(newFrame(cmdSYN, sid))
@@ -147,11 +150,12 @@ func (s *Session) streamClosed(sid uint32) {
 // nonblocking read from session pool, for streams
 func (s *Session) nioread(sid uint32, p []byte) (n int) {
 	s.streamLock.Lock()
-	n = copy(p, s.streamBuffers[sid])
-	if n > 0 {
-		s.streamBuffers[sid] = s.streamBuffers[sid][n:]
-		atomic.AddInt32(&s.bucket, int32(n))
-		s.bucketCond.Signal()
+	if streamBuffer, ok := s.streamBuffers[sid]; ok {
+		n, _ = streamBuffer.Read(p)
+		if n > 0 {
+			atomic.AddInt32(&s.bucket, int32(n))
+			s.bucketCond.Signal()
+		}
 	}
 	s.streamLock.Unlock()
 	return
@@ -187,7 +191,7 @@ func (s *Session) monitor() {
 		case sid := <-s.chClosedStream:
 			s.streamLock.Lock()
 			delete(s.streams, sid)
-			if n := len(s.streamBuffers[sid]); n > 0 { // return remaining tokens to the bucket
+			if n := s.streamBuffers[sid].Len(); n > 0 { // return remaining tokens to the bucket
 				atomic.AddInt32(&s.bucket, int32(n))
 				s.bucketCond.Signal()
 			}
@@ -225,7 +229,9 @@ func (s *Session) recvLoop() {
 				s.streamLock.Lock()
 				if _, ok := s.streams[f.sid]; !ok {
 					stream := newStream(f.sid, s.config.MaxFrameSize, s)
+					streamBuffer := new(bytes.Buffer)
 					s.streams[f.sid] = stream
+					s.streamBuffers[f.sid] = streamBuffer
 					go func() { s.chAccepts <- stream }()
 				} else { // stream exists, RST the peer
 					go s.writeFrame(newFrame(cmdRST, f.sid))
@@ -242,7 +248,7 @@ func (s *Session) recvLoop() {
 				s.streamLock.Lock()
 				if stream, ok := s.streams[f.sid]; ok {
 					atomic.AddInt32(&s.bucket, -int32(len(f.data)))
-					s.streamBuffers[f.sid] = append(s.streamBuffers[f.sid], f.data...)
+					s.streamBuffers[f.sid].Write(f.data)
 					stream.notifyReadEvent()
 				} else { // stream is absent
 					go s.writeFrame(newFrame(cmdRST, f.sid))
