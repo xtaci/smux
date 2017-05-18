74c4ad63ca7b21d6389363eb76969ac4ab541bee
cs8425 <cs8425@gmail.com>
2017-05-19 01:16:47 +0800

    fix close before read out all data

--- stream.go
+++ stream.go
@@ -73,8 +73,6 @@ func (s *Stream) Read(b []byte) (n int, err error) {
 
 READ:
 	select {
-	case <-s.die:
-		return 0, errors.New(errBrokenPipe)
 	case <-deadline:
 		return n, errTimeout
 	default:
