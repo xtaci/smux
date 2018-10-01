8edaea72f5ba0e6958ec43d3d897db4da8e262ec
xtaci <daniel820313@gmail.com>
2018-10-01 11:16:18 +0800

    fix a test

--- session_test.go
+++ session_test.go
@@ -539,8 +539,8 @@ func TestReadDeadline(t *testing.T) {
 	var readErr error
 	for i := 0; i < N; i++ {
 		msg := fmt.Sprintf("hello%v", i)
-		stream.Write([]byte(msg))
 		stream.SetReadDeadline(time.Now().Add(-1 * time.Minute))
+		stream.Write([]byte(msg))
 		if _, readErr = stream.Read(buf); readErr != nil {
 			break
 		}
